3d22572698b2f9027bbcc1314464f1e87b639872
Terentev Nikita <nitro-2003@mail.ru>
2024-02-18 14:05:25 +0700

    Added transparent to search field

--- app/src/main/java/com/example/locmes/MainActivity.java
+++ app/src/main/java/com/example/locmes/MainActivity.java
@@ -1,23 +1,15 @@
 package com.example.locmes;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.RenderEffect;
-import android.graphics.Shader;
-import android.os.Bundle;
-import android.widget.LinearLayout;
-
 public class MainActivity extends AppCompatActivity {
-
-    LinearLayout layoutSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        layoutSearch = findViewById(R.id.layoutSearch);
-
 
     }
 }
