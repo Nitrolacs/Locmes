2831c42e939a9ac4743d37870d28993c69e74730
Terentev Nikita <nitro-2003@mail.ru>
2024-03-02 13:05:13 +0700

    Added calendar

--- app/src/main/java/com/example/locmes/activities/AddToCalendar.java
+++ app/src/main/java/com/example/locmes/activities/AddToCalendar.java
@@ -3,18 +3,53 @@ package com.example.locmes.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.locmes.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AddToCalendar extends AppCompatActivity {
+
+    CalendarView calendarView;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_calendar);
 
+        calendarView = findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+
+        calendarView.setOnDateChangeListener((calendarView, year, month, day) -> Toast.makeText(AddToCalendar.this, day + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT).show());
+
+        setDate(2, 3, 2024);
+        getDate();
+
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(view -> onBackPressed());
     }
+
+    public void setDate(int day, int month, int year) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        long milli = calendar.getTimeInMillis();
+        calendarView.setDate(milli);
+    }
+
+    public void getDate() {
+        long date = calendarView.getDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
+        calendar.setTimeInMillis(date);
+        String selected_date = simpleDateFormat.format(calendar.getTime());
+        Toast.makeText(this, selected_date, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
--- app/src/main/java/com/example/locmes/activities/CalendarActivity.java
+++ app/src/main/java/com/example/locmes/activities/CalendarActivity.java
@@ -11,25 +11,38 @@ import android.graphics.Shader;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.locmes.R;
 import com.google.android.material.navigation.NavigationView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CalendarActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
+    CalendarView calendarView;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            findViewById(R.id.backgroundImageBlurred).setRenderEffect(RenderEffect.createBlurEffect(80, 80, Shader.TileMode.CLAMP));
-        }
+        calendarView = findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+
+        calendarView.setOnDateChangeListener((calendarView, year, month, day) -> Toast.makeText(CalendarActivity.this, day + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT).show());
+
+        setDate(2, 3, 2024);
+        getDate();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            findViewById(R.id.backgroundImageBlurred).setRenderEffect(RenderEffect.createBlurEffect(80, 80, Shader.TileMode.CLAMP));
             findViewById(R.id.backgroundWhiteImageBlurred).setRenderEffect(RenderEffect.createBlurEffect(80, 80, Shader.TileMode.CLAMP));
         }
 
@@ -47,6 +60,22 @@ public class CalendarActivity extends AppCompatActivity implements NavigationVie
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+    }
+
+    public void setDate(int day, int month, int year) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        long milli = calendar.getTimeInMillis();
+        calendarView.setDate(milli);
+    }
+
+    public void getDate() {
+        long date = calendarView.getDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
+        calendar.setTimeInMillis(date);
+        String selected_date = simpleDateFormat.format(calendar.getTime());
+        Toast.makeText(this, selected_date, Toast.LENGTH_SHORT).show();
     }
 
     @Override
